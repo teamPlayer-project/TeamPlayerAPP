948169ab371bf8d29f2648e3e488810b078b2b5b
Nofar Bar Zakai <nofar.barzakai@live.biu.ac.il>
2020-03-01 20:49:04 +0200

    screen 2

---
+++ app/src/main/java/com/example/teamplayer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.teamplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        Intent intent=new Intent(this,Select_actions.class);
+        //Send the ip and port to the joystick
+        startActivity(intent);
+    }
+
+}
