31e2681fc6f5bec306ca6cf2fbd876fb76ebb57e
Reut Baruch <reut.baruch@live.biu.ac.il>
2020-03-03 15:20:52 +0200

    Reut hayta po

---
+++ app/src/main/java/com/teamplayer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.teamplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int a = 8;
+        setContentView(R.layout.activity_main);
+    }
+}
